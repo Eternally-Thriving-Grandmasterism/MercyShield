85f99a3f83dc43d995f901a11595253a094b66ec
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-07 22:30:03 -0500

    Update main.py

--- mercy_shield/main.py
+++ mercy_shield/main.py
@@ -1,4 +1,8 @@
 from mercy_shield.ui import MercyApp
+from mercy_shield.shield import RealTimeShield
+from mercy_shield.lattice import MercyLattice
 
 if __name__ == "__main__":
-    MercyApp().run()
+    lattice = MercyLattice(threads=13)
+    shield = RealTimeShield(lattice)
+    MercyApp(shield).run()
