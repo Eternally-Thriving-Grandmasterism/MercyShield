ff7e1621567a67e1e8a8ac4a4cb5213d009570e7
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-08 23:01:46 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,59 @@
 use pyo3::prelude::*;
 use pqcrypto_kyber::kyber1024::*;
-use pqcrypto_traits::kem::{Ciphertext, PublicKey, SecretKey, SharedSecret};
+use pqcrypto_dilithium::dilithium3::*;
+use pqcrypto_traits::kem::{Ciphertext, PublicKey as KemPk, SecretKey as KemSk, SharedSecret};
+use pqcrypto_traits::sign::{PublicKey as SignPk, SecretKey as SignSk, SignedMessage, Signature};
 
+// Kyber KEM functions (previous mercy)
 #[pyfunction]
-fn generate_keypair() -> (Vec<u8>, Vec<u8>) {
+fn kyber_keypair() -> (Vec<u8>, Vec<u8>) {
     let (pk, sk) = keypair();
     (pk.as_bytes().to_vec(), sk.as_bytes().to_vec())
 }
 
 #[pyfunction]
-fn encapsulate(pk_bytes: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
-    let pk = PublicKey::from_bytes(&pk_bytes).unwrap();
+fn kyber_encapsulate(pk_bytes: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
+    let pk = KemPk::from_bytes(&pk_bytes).unwrap();
     let (ss, ct) = encapsulate(&pk);
     (ss.as_bytes().to_vec(), ct.as_bytes().to_vec())
 }
 
 #[pyfunction]
-fn decapsulate(sk_bytes: Vec<u8>, ct_bytes: Vec<u8>) -> Vec<u8> {
-    let sk = SecretKey::from_bytes(&sk_bytes).unwrap();
+fn kyber_decapsulate(sk_bytes: Vec<u8>, ct_bytes: Vec<u8>) -> Vec<u8> {
+    let sk = KemSk::from_bytes(&sk_bytes).unwrap();
     let ct = Ciphertext::from_bytes(&ct_bytes).unwrap();
     let ss = decapsulate(&sk, &ct);
     ss.as_bytes().to_vec()
 }
 
+// Dilithium3 Signature functions divine
+#[pyfunction]
+fn dilithium_keypair() -> (Vec<u8>, Vec<u8>) {
+    let (pk, sk) = keypair();
+    (pk.as_bytes().to_vec(), sk.as_bytes().to_vec())
+}
+
+#[pyfunction]
+fn dilithium_sign(sk_bytes: Vec<u8>, message: Vec<u8>) -> Vec<u8> {
+    let sk = SignSk::from_bytes(&sk_bytes).unwrap();
+    let signed = sign(&message, &sk);
+    signed.as_bytes().to_vec()
+}
+
+#[pyfunction]
+fn dilithium_verify(pk_bytes: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+    let pk = SignPk::from_bytes(&pk_bytes).unwrap();
+    let signed = SignedMessage::from_bytes(&signature).unwrap();
+    verify(&signed, &message, &pk).is_ok()
+}
+
 #[pymodule]
 fn mercy_pqc(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(generate_keypair, m)?)?;
-    m.add_function(wrap_pyfunction!(encapsulate, m)?)?;
-    m.add_function(wrap_pyfunction!(decapsulate, m)?)?;
+    m.add_function(wrap_pyfunction!(kyber_keypair, m)?)?;
+    m.add_function(wrap_pyfunction!(kyber_encapsulate, m)?)?;
+    m.add_function(wrap_pyfunction!(kyber_decapsulate, m)?)?;
+    m.add_function(wrap_pyfunction!(dilithium_keypair, m)?)?;
+    m.add_function(wrap_pyfunction!(dilithium_sign, m)?)?;
+    m.add_function(wrap_pyfunction!(dilithium_verify, m)?)?;
     Ok(())
 }
